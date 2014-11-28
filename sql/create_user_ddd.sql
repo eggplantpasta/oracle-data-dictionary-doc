d351cd2e2a04571e2e91167fc9eefac8bab25966
Brett Roper <roper.brett@gmail.com>
2014-11-28 21:21:27 +0800

    tweaked create_user

--- sql/create_user_ddd.sql
+++ sql/create_user_ddd.sql
@@ -1,15 +1,16 @@
 -- use if creating package and directory in a separate schema to the one being documented
 -- modify as appropriate for your environment
 
--- start run as as privileged user e.g. system
--- drop user ddd cascade;
+-- run as sys
+drop user ddd cascade;
 create user ddd identified by ddd;
 grant connect, resource to ddd;
 grant create database link, create procedure to ddd;
 grant create any directory to ddd; -- allows user to read and write to the filesystem of the db server
 grant select any dictionary to ddd; -- allows user to select from dba_* tables
+grant execute on utl_file to ddd;
 
 -- connection to our newly created user to create the package and directory
-connect ddd/ddd@xe;
-create or replace directory ddd_files as 'C:\gitrepo\oracle-data-dictionary-doc\files';
+connect ddd/ddd@localhost;
+create or replace directory ddd_files as '/files';
 @ddd.sql
